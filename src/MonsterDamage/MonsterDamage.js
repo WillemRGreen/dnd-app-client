8d7f32a4d863a0e4de3d139a415d0daaae16259f
Will Green <willgreen@Wills-MacBook-Pro.local>
2021-09-20 14:18:45 -0500

    began damage calc

--- src/MonsterDamage/MonsterDamage.js
+++ src/MonsterDamage/MonsterDamage.js
@@ -2,9 +2,44 @@ import React, { Component } from 'react'
 import GenericForm from '../GenericForm/GenericForm'
 
 export default class MonsterDamage extends Component {
+    static defaultProps = {
+        history: {
+          push: () => { }
+        },
+      }
+
+      state = {
+          party: [],
+          enemies: []
+      }
+
+      createPartyMember (attackBonus, numOfAttacks, damageDie, damageBonus, hitPoints){
+        let newPartyMember = {
+            attackBonus,
+            numOfAttacks,
+            damageDie,
+            damageBonus,
+            hitPoints
+        }
+        this.setState({party: [...this.this.state.party, newPartyMember]})
+      }
+
+      createEnemy (attackBonus, numOfAttacks, damageDie, damageBonus, hitPoints){
+        let newEnemy = {
+            attackBonus,
+            numOfAttacks,
+            damageDie,
+            damageBonus,
+            hitPoints
+        }
+        this.setState({party: [...this.this.state.enemies, newEnemy]})
+      }
+
     render() {
         return (
-            undefined
+            <section>
+                
+            </section>
         )
     }
 }
